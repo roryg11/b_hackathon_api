906f8057726f3a7b48615b05ee7f6bd1860b72a3
Rory Grant <rory@beckon.com>
2017-03-02 16:52:01 -0800

    readProperties for setting env

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var request = require('request');
 var google = require('googleapis');
+var readProperties = require('./services/readPropertiesService.js')();
 
 // configuration
 app.use(express.static(__dirname + '/public'));
@@ -22,8 +23,8 @@ app.use(methodOverride());
           url: "https://app.beckon.com/rest/a/QA/datasummary/basic",
           method: "GET",
           headers: {
-              Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
-              "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a"
+              Cookie: readProperties["com.beckon.jSessionId"],
+              "X-XSRF-TOKEN": readProperties["com.beckon.xsrfToken"]
           }
       };
     request(options, function(error, response, body){
@@ -39,8 +40,8 @@ app.use(methodOverride());
             url: "https://app.beckon.com/rest/a/QA/dashboard/list",
             method: "GET",
             headers: {
-                Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
-                "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a",
+                Cookie: readProperties["com.beckon.jSessionId"],
+                "X-XSRF-TOKEN": readProperties["com.beckon.xsrfToken"],
                 "Content-Type": "application/json",
                 "Accept": "application/json, text/plain, */*"
             }
@@ -59,8 +60,8 @@ app.get("/api/scorecards", function(req, res){
         url: "https://app.beckon.com/rest/a/QA/scorecards",
         method: "GET",
         headers: {
-            Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
-            "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a",
+            Cookie: readProperties["com.beckon.jSessionId"],
+            "X-XSRF-TOKEN": readProperties["com.beckon.xsrfToken"],
             "Content-Type": "application/json",
             "Accept": "application/json, text/plain, */*"
         }
