61dce6bd10f260159cd6428ed767d0b311dea29c
dana <dana@beckon.com>
2017-03-03 09:43:12 -0800

    add chart for minutes used and animate

--- public/core.js
+++ public/core.js
@@ -63,34 +63,61 @@
       });
     };
 
+    function growBar(elem, totalWidth) {
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (width >= totalWidth) {
+          clearInterval(id);
+        } else {
+          width++;
+          elem.style.width = width + '%';
+        }
+      }
+    }
+
+    var secondFrameTrigger = false;
 
 
     var zeroPanel = angular.element.find("#frame1")[0].offsetTop;
 
     angular.element($window).bind("scroll", function(e) {
-      if ($window.pageYOffset > zeroPanel && $window.pageYOffset < zeroPanel + 500) {
-        console.log("in first frame");
-      } else if ($window.pageYOffset > zeroPanel + 500 && $window.pageYOffset < zeroPanel + 500*2) {
-        console.log("in second frame");
-        //fill hours at beckon chart
-        angular.element('progress-fill').each(function(){
-          var minuteValue = this.html();
-          var value = minuteValue.split(0, minuteValue.length- 8);
-          this.parent().css('width', value);
-        });
-      } else if ($window.pageYOffset > zeroPanel + 500*2 && $window.pageYOffset < zeroPanel + 500*3) {
-        console.log("in third frame");
-      } else if ($window.pageYOffset > zeroPanel + 500*3 && $window.pageYOffset < zeroPanel + 500*4) {
-        console.log("in fourth frame");
-      } else if ($window.pageYOffset > zeroPanel + 500*4 && $window.pageYOffset < zeroPanel + 500*5) {
-        console.log("in fifth frame");
-      } else if ($window.pageYOffset > zeroPanel + 500*5 && $window.pageYOffset < zeroPanel + 500*6) {
-        console.log("in sixth frame");
-      } else if ($window.pageYOffset > zeroPanel + 500*6 && $window.pageYOffset < zeroPanel + 500*7) {
-        console.log("in seventh frame");
-      } else if ($window.pageYOffset > zeroPanel + 500*7 && $window.pageYOffset < zeroPanel + 500*8) {
-        console.log("in eigth frame");
-      }
+
+       if ($window.pageYOffset < zeroPanel - 500) {
+         secondFrameTrigger = false;
+         console.log("below first frame");
+       } else if ($window.pageYOffset > zeroPanel - 500 && $window.pageYOffset < zeroPanel) {
+         console.log("in first frame");
+       } else if ($window.pageYOffset > zeroPanel && $window.pageYOffset < zeroPanel + 500 ) {
+         console.log("in second frame");
+         //fill hours at beckon chart
+         if (!secondFrameTrigger) {
+           if (angular.element('.progress-fill span').length) {
+             var innerTextValue = angular.element('.progress-fill span')[0].innerText;
+             var greatestValue = innerTextValue.substr(0, innerTextValue.length - 8);
+             angular.element('.progress-fill span').each(function(){
+               var minuteValue = this.innerText;
+               var percentFill = ((minuteValue.substr(0, minuteValue.length - 8)/greatestValue) * 100);
+               growBar(this.parentElement, percentFill);
+             });
+             secondFrameTrigger = true;
+           }
+         }
+
+       } else if ($window.pageYOffset > zeroPanel + 500 && $window.pageYOffset < zeroPanel + 500*2) {
+         console.log("in third frame");
+       } else if ($window.pageYOffset > zeroPanel + 500*2 && $window.pageYOffset < zeroPanel + 500*3) {
+         console.log("in fourth frame");
+       } else if ($window.pageYOffset > zeroPanel + 500*3 && $window.pageYOffset < zeroPanel + 500*4) {
+         console.log("in fifth frame");
+       } else if ($window.pageYOffset > zeroPanel + 500*4 && $window.pageYOffset < zeroPanel + 500*5) {
+         console.log("in sixth frame");
+       } else if ($window.pageYOffset > zeroPanel + 500*5 && $window.pageYOffset < zeroPanel + 500*6) {
+         console.log("in seventh frame");
+       } else if ($window.pageYOffset > zeroPanel + 500*6 && $window.pageYOffset < zeroPanel + 500*7) {
+         console.log("in eigth frame");
+       }
+
     });
 
     // var previouslyAppliedColor = "color-white";
