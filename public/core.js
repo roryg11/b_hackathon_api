bb1574f9b6bf56dd0925beec69a17786de1f5a84
Rory Grant <rory@beckon.com>
2017-03-02 16:26:29 -0800

    changes so far

--- public/core.js
+++ public/core.js
@@ -1,11 +1,72 @@
-var todoApp = angular.module('todoApp', []);
+(function(){
+  var todoApp = angular.module('todoApp', [
 
-function mainController($scope, $http){
-  $scope.getData = function(){
-    $http.get('/api/getSummary').success(function(data){
-      $scope.dataSummary = data;
-    }).error(function(data){
-      console.log('Error: ' + data);
-    });
-  }
-}
+  ]).controller('mainController', function($scope, $http, $window){
+    var dateOneYearAgo = new Date('March 3 2016').getTime();
+    var dateToday = new Date().getTime();
+    function getTopDashboardOwners (dashboardsList){
+      var dashboardOwnerMap = {};
+      for(var i =0; i < dashboardsList.length; i++){
+        var name = dashboardsList[i].userPermissions.owner.firstName + " " + dashboardsList[i].userPermissions.owner.lastName;
+        dashboardOwnerMap[name] += 1;
+      }
+      var sortable = [];
+      for(var nameKey in dashboardOwnerMap){
+        sortable.push([nameKey, dashboardOwnerMap[nameKey]]);
+      }
+      return sortable;
+    }
+
+    function createdThisYear (entity){
+      return entity.dateAdded > dateOneYearAgo && entity.dateAdded < dateToday;
+    }
+
+    $scope.getData = function(){
+      $http.get('/api/getSummary').then(function(response){
+        $scope.dataSummary = response.data;
+      }, function(data){
+        console.log('Error: ' + data);
+      });
+    };
+
+    $scope.getDashboardsList = function(){
+      $http.get('/api/dashboards').then(function(response){
+        $scope.dashboardSummary = response.data.filter(createdThisYear);
+        $scope.numberOfDashboards = $scope.dashboardSummary.length;
+        $scope.masterOwners = getTopDashboardOwners($scope.dashboardSummary);
+      }, function(data){
+        console.log('Error: ' + data);
+      });
+    };
+
+    $scope.getScorecardsList = function(){
+      $http.get('/api/scorecards').then(function(data){
+        $scope.scorecardSummary = response.data;
+      }, function(data){
+        console.log('Error: ' + data);
+      });
+    };
+
+    var previouslyAppliedColor = "color-white";
+
+    // angular.element($window).bind("scroll", function(e) {
+    //   var scroll = $window.pageYOffset + ($window.innerHeight/3);
+    //   var elementColors = angular.element.find('.element-color');
+    //   function classToRemove (){
+    //
+    //   }
+    //   for(var i=0; i < elementColors.length; i++){
+    //     if(elementColors[i].offsetTop <=scroll && elementColors[i].offsetTop + elementColors[i].offsetHeight > scroll){
+    //       angular.element.find('.container-to-recolor')[0].classList.remove(previouslyAppliedColor);
+    //       var color = "color-" + elementColors[i].getAttribute('data-color');
+    //       angular.element.find('.container-to-recolor')[0].classList.add(color);
+    //       previouslyAppliedColor = color;
+    //     }
+    //   }
+    //
+    // });
+  });
+
+
+
+})();
--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(methodOverride());
 
   app.get("/api/getSummary", function(req, res){
       var options = {
-          url: "",
+          url: "https://app.beckon.com/rest/a/QA/datasummary/basic",
           method: "GET",
           headers: {
-              Cookie: "",
-              "X-XSRF-TOKEN": ""
+              Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
+              "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a"
           }
       };
     request(options, function(error, response, body){
@@ -32,6 +32,46 @@ app.use(methodOverride());
         res.send(body);
     });
   });
+
+    app.get("/api/dashboards", function(req, res){
+        var options = {
+            url: "https://app.beckon.com/rest/a/QA/dashboard/list",
+            method: "GET",
+            headers: {
+                Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
+                "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a",
+                "Content-Type": "application/json",
+                "Accept": "application/json, text/plain, */*"
+            }
+        };
+        request(options, function(error, response, body){
+            if(error){
+                console.log(error);
+            }
+            console.log(response);
+            res.send(body);
+        });
+    });
+
+app.get("/api/scorecards", function(req, res){
+    var options = {
+        url: "https://app.beckon.com/rest/a/QA/scorecards",
+        method: "GET",
+        headers: {
+            Cookie: "JSESSIONID=10_10_102_22_7k6bzw45dy2xru84py20dsi4.10_10_103_104_",
+            "X-XSRF-TOKEN": "5376257b22be41c70578007daf6522da7126f20a",
+            "Content-Type": "application/json",
+            "Accept": "application/json, text/plain, */*"
+        }
+    };
+    request(options, function(error, response, body){
+        if(error){
+            console.log(error);
+        }
+        console.log(response);
+        res.send(body);
+    });
+});
 
   // application -------------------------------------
   app.get("/home", function(req,res){
