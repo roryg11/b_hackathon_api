d19865ad85bef2c317a21d8b4cfe8d79b2cedbed
Rory Grant <rory@beckon.com>
2017-03-02 17:32:23 -0800

    getting numbers right

--- public/core.js
+++ public/core.js
@@ -4,21 +4,29 @@
   ]).controller('mainController', function($scope, $http, $window){
     var dateOneYearAgo = new Date('March 3 2016').getTime();
     var dateToday = new Date().getTime();
-    function getTopDashboardOwners (dashboardsList){
-      var dashboardOwnerMap = {};
-      for(var i =0; i < dashboardsList.length; i++){
-        var name = dashboardsList[i].userPermissions.owner.firstName + " " + dashboardsList[i].userPermissions.owner.lastName;
-        dashboardOwnerMap[name] += 1;
+    function getTopOwners (entityList){
+      var ownerMap = {};
+      for(var i =0; i < entityList.length; i++){
+        var name = entityList[i].userPermissions.owner.firstName + " " + entityList[i].userPermissions.owner.lastName;
+        if(ownerMap[name]){
+          ownerMap[name] = ownerMap[name] +1;
+        } else {
+          ownerMap[name]= 1;
+        }
       }
       var sortable = [];
-      for(var nameKey in dashboardOwnerMap){
-        sortable.push([nameKey, dashboardOwnerMap[nameKey]]);
+      for(var nameKey in ownerMap){
+        sortable.push([nameKey, ownerMap[nameKey]]);
       }
       return sortable;
     }
 
     function createdThisYear (entity){
-      return entity.dateAdded > dateOneYearAgo && entity.dateAdded < dateToday;
+      if(entity.type === "SCORECARD_DTO"){
+        return entity.createdAt > dateOneYearAgo && entity.createdAt < dateToday;
+      } else {
+        return entity.dateAdded > dateOneYearAgo && entity.dateAdded < dateToday;
+      }
     }
 
     $scope.getData = function(){
@@ -33,15 +41,17 @@
       $http.get('/api/dashboards').then(function(response){
         $scope.dashboardSummary = response.data.filter(createdThisYear);
         $scope.numberOfDashboards = $scope.dashboardSummary.length;
-        $scope.masterOwners = getTopDashboardOwners($scope.dashboardSummary);
+        $scope.masterOwners = getTopOwners($scope.dashboardSummary);
       }, function(data){
         console.log('Error: ' + data);
       });
     };
 
     $scope.getScorecardsList = function(){
-      $http.get('/api/scorecards').then(function(data){
-        $scope.scorecardSummary = response.data;
+      $http.get('/api/scorecards').then(function(response){
+        $scope.scorecardSummary = response.data.filter(createdThisYear);
+        $scope.numberOfScorecards = $scope.scorecardSummary.length;
+        $scope.scorecardSamurai = getTopOwners($scope.scorecardSummary);
       }, function(data){
         console.log('Error: ' + data);
       });
